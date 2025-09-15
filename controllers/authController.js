7b0e7c56bcdafde5ea56717c611820d12e4fd8c3
NaijaTechBro <bakisodiq@gmail.com>
2025-09-15 03:26:31 +0100

    update: added organization number

--- controllers/authController.js
+++ controllers/authController.js
@@ -74,9 +74,9 @@ const sendTokenResponse = async (user, statusCode, res) => {
 // @route     POST /api/v1/auth/register
 // @access    Public
 const registerUser = asyncHandler(async (req, res) => {
-    const { firstName, lastName, email, password, organization, phoneNumber, role } = req.body;
+    const { firstName, lastName, email, password, organization, role } = req.body;
 
-    if (!email || !password || !firstName || !lastName || !phoneNumber || !organization || !organization.name || !organization.industry || !organization.address || !organization.phoneNumber || !organization.website ) {
+    if (!email || !password || !firstName || !lastName || !organization || !organization.name || !organization.industry || !organization.address || !organization.phoneNumber || !organization.website ) {
         res.status(400);
         throw new Error('Please provide all required user and organization fields, including address, phone, and website.');
       }
