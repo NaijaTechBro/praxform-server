520d3ab692f923c3e3b857c4640fc5a35ec18059
NaijaTechBro <bakisodiq@gmail.com>
2025-09-15 04:25:21 +0100

    update: editing organization

--- routes/organizationRoutes.js
+++ routes/organizationRoutes.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const { getOrganizationById, updateOrganization } = require('../controllers/organizationController');
 const { protect } = require('../middleware/authMiddleware');
 
-router.route('/:id').get(protect, getOrganizationById).put(protect, updateOrganization);
+
+router.route('/:id')
+    .get(protect, getOrganizationById)
+    .put(protect, updateOrganization);
 
 module.exports = router;
