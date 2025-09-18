5d5caf31bba64608b764645cfbb2028aac0f4746
NaijaTechBro <bakisodiq@gmail.com>
2025-09-18 02:30:08 +0100

    email authentication

--- routes/submissionRoutes.js
+++ routes/submissionRoutes.js
@@ -4,18 +4,19 @@ const {
     createSubmission,
     getSubmissionById,
     deleteSubmission,
+    getSubmissionsByForm,
     getPublicFormByAccessCode,
-    verifySubmission 
+    verifyAccessAndGetForm, 
 } = require('../controllers/submissionController');
 const { protect } = require('../middleware/authMiddleware');
 const audit = require('../middleware/auditMiddleware');
 
 router.post('/', createSubmission);
-router.post('/verify', verifySubmission); 
+router.post('/verify-access', verifyAccessAndGetForm);
 
 router.get('/:id/:accessCode', getPublicFormByAccessCode);
 
-
+router.get('/form/:id', protect, getSubmissionsByForm);
 router.route('/:id')
     .get(protect, audit('submission.viewed', 'submission'), getSubmissionById)
     .delete(protect, audit('submission.deleted', 'submission'), deleteSubmission);
